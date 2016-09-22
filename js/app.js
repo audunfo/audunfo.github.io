9cd77da41ca2b1b483829f38881a7c0a8a234bba
Audun Follegg <afo@telenordigital.com>
2016-09-22 16:29:15 +0200

    test

---
+++ js/app.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function () {
+  // var app = angular.module('dotmutual', ['ngMaterial']);
+  //
+  // app.controller('MainCtrl', function(){
+  //   console.log('Hello');
+  // });
+
+  console.log('im running');
+}());
