5e7c08a66ae584ea1eef23dd68deece99fce2c05
Audun Follegg <afo@telenordigital.com>
2016-09-22 17:35:34 +0200

    Add some structure

--- js/app.js
+++ js/app.js
@@ -1,11 +1,10 @@
 'use strict';
-
 (function () {
-  // var app = angular.module('dotmutual', ['ngMaterial']);
-  //
-  // app.controller('MainCtrl', function(){
-  //   console.log('Hello');
-  // });
-
+  angular.module('dotMutual', ['ngMaterial'])
+    .controller('AppCtrl', function($scope){
+      var vc = this;
+      vc.title = "test";
+      console.log('Hello from controller')
+    });
   console.log('im running');
 }());
